aa2fae0b3404c43e88d291899dea8c07a7fccf11
heejung-choi <choi951008@naver.com>
2020-07-23 15:49:28 +0900

    cnt 모델 삭제

--- kculture/enters/models.py
+++ kculture/enters/models.py
@@ -5,12 +5,9 @@ from maps.models import Spot
 class Show(models.Model):
     group_name = models.CharField(max_length=500)
     group_img = models.CharField(max_length=500)
-    group_cnt = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user_show', null=True)
-   
 
 class Showman(models.Model):
     showman_name = models.CharField(max_length=500)
     showman_img = models.CharField(max_length=500)
     show_cd = models.ForeignKey(Show, on_delete=models.CASCADE, null=True)
-    showman_cnt = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user_showman', null=True)
     spot_cd = models.ManyToManyField(Spot, related_name='spot_show', null=True)
--- kculture/singers/models.py
+++ kculture/singers/models.py
@@ -5,12 +5,10 @@ from maps.models import Spot
 class Group(models.Model):
     group_name = models.CharField(max_length=500)
     group_img = models.CharField(max_length=500)
-    group_cnt = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user_group', null=True)
     spot_cd = models.ManyToManyField(Spot, related_name='spot_group')
 
 class Singer(models.Model):
     singer_name = models.CharField(max_length=500)
     singer_img = models.CharField(max_length=500)
     group_cd = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
-    singer_cnt = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user_singer', null=True)
     spot_cd = models.ManyToManyField(Spot, related_name='spot_singer')
