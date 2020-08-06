3bbb1b7822bdffd62b72574cdb39069ea580a6f5
eunnaPark1115 <eunnaPark1115@naver.com>
2020-08-06 15:18:59 +0900

    eunhapart merge

--- dasoni/accounts/urls.py
+++ dasoni/accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    path('test1/', views.test1, name='test1'),
+   
     # path('logout/', views.logout, name='logout'),
     # path('mypage/', views.update, name='update'),
 ]
