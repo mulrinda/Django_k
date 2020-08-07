9dbce9d29eebaee1a4600ad2d786a1d8d45e0ff3
eunnaPark1115 <eunnaPark1115@naver.com>
2020-08-07 18:01:49 +0900

    eunha first spotrank

--- dasoni/trips/urls.py
+++ dasoni/trips/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'trips'
 
 urlpatterns = [
-    
+    path('spotchart', views.spotchart, name='spotchart'),
 ]
--- dasoni/trips/views.py
+++ dasoni/trips/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def spotchart(request):
+    return render(request, 'trips/spotchart.html')
