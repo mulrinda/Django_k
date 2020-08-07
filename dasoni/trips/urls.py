dfa2864022c3d0f1264167de2b6a1072429054e6
GyuyoungEom <linda_jenny@naver.com>
2020-08-07 16:08:28 +0900

    create md files

--- dasoni/trips/urls.py
+++ dasoni/trips/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'trips'
 
 urlpatterns = [
-    
+    path('share/', views.share, name='share'),
+    path('create/', views.create, name='create'),
 ]
--- dasoni/trips/views.py
+++ dasoni/trips/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+def share(request):
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts
+    }
+    return render(request,'trips/share.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('trips:share')
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'trips/share_create.html', context)
