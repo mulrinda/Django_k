d4053f9bfd2adae3d269a1b0c626f061e2ed8046
stella <smmin0402@gmail.com>
2020-08-10 17:14:46 +0900

    theme tour_url/view

--- dasoni/tothers/urls.py
+++ dasoni/tothers/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'tothers'
 
 urlpatterns = [
+    path('',views.index,name="index"),
     path('goods', views.goods, name='goods'),
     path('tourtip', views.tourtip, name='tourtip'),
+    path('theme/<str:cont>/<str:lang>/', views.theme, name='theme'), # semi(0803) : theme개별페이지 테스트중
 ]
--- dasoni/tothers/views.py
+++ dasoni/tothers/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
+from urllib.parse import urlparse
+import pandas as pd
+import json
+import requests
 
 def goods(request):
     return render(request,'tothers/goods.html')
 
 def tourtip(request):
     return render(request,'tothers/tourtip.html')
+
+def theme(request,cont,lang):
+    url='tothers/theme_'+cont+'_'+lang+'.html'
+    return render(request, url)
+
+addr='서울특별시 중구 퇴계로34길 28'
+def index(request):
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
+    headers = {"Authorization": "KakaoAK 073a393656181c6073880062d3507191"}
+    result = json.loads(str(requests.get(url, headers=headers).text))
+    match_first = result['documents'][0]['address']
+    print(float(match_first['y']), float(match_first['x']))
+    return render(request, 'tothers/index.html')
