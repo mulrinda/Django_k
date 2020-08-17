36f7266aac06d503cc04983a6a41ccfc13aa4d7b
heejung-choi <choi951008@naver.com>
2020-08-17 15:47:39 +0900

    carts admin add

--- dasoni/carts/admin.py
+++ dasoni/carts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
+from .models import Product, Cartitem, Cart
 # Register your models here.
+class ProductAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+class CartitemAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+class CartAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cartitem, CartitemAdmin)
+admin.site.register(Cart, CartAdmin)
